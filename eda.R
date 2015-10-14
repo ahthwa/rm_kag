7ff173318634fdaa36a57bd3af6ad943b71081f9
YJ Hong <ahthwa@gmail.com>
2015-10-14 19:14:34 +0900

    eda 재구성

--- eda.R
+++ eda.R
@@ -1,21 +1,77 @@
 library("data.table")
 library("ggplot2")
+library("zoo")
 
 #https://www.kaggle.com/thie1e/rossmann-store-sales/exploratory-analysis-rossmann/notebook 세세한 팁. 이거 보고 배울 것.
 
-train_data <- fread("data/train.csv", sep=",", header = T)
-test_data <- fread("data/test.csv", sep=",", header = T)
-store_data <- fread("data/store.csv", sep=",", header = T)
-train_data = cbind(train_data, poxis_dt  = strptime(train_data$Date, "%Y-%m-%d"))
+train <- fread("data/train.csv", sep=",", header = T)
+test <- fread("data/test.csv", sep=",", header = T)
+store <- fread("data/store.csv", sep=",", header = T)
 
-summary(train_data)
-summary(test_data)
-summary(store_data)
+train$Date = as.Date(train$Date)
+test$Date = as.Date(test$Date)
 
-head(train_data)
-head(store_data)
+summary(train)
+summary(test)
+summary(store)
 
-# Sales 분포
+head(train)
+head(store)
+
+# 날짜순으로 Train, Test 정렬
+train <- train[order(train$Date)] 
+test <- test[order(test$Date)]
+
+# Train Data
+## Store
+length(unique(train$Store))
+
+## 누락된 날짜가 있는가
+max(train$Date) - min(train$Date) + 1
+length(unique(train$Date))
+
+## 날짜 순 정렬이 잘 되었나
+plot(train$Date, type = 'l')
+plot(test$Date, type='l')
+
+## 일별 데이터 건수의 차이가 있는가
+## 2014년 7월부터 2015년 1월 까지 데이터 양이 적다.
+ggplot(train) + geom_histogram(aes(x = Date))
+ggplot(test) + geom_histogram(aes(x = Date))
+
+## 요일별 데이터 건수
+table(train$DayOfWeek)
+table(train$DayOfWeek) / nrow(train)
+table(test$DayOfWeek)
+table(test$DayOfWeek) / nrow(test)
+
+## Sales
+## sales 0인 애들이 꽤 있음.
+ggplot(train) + geom_histogram(aes(x = Sales), binwidth=500)
+## sales 0빼고
+ggplot(train[Sales != 0]) + geom_histogram(aes(x = Sales), binwidth=500)
+## 상점별 평균 Sales
+ggplot(train[Sales != 0, .(meanSales = mean(Sales)), by = (Store)]) +
+  geom_histogram(aes(x = meanSales), binwidth = 500)
+## 요일별 sales
+ggplot(train[Sales != 0, .(sumSales = sum(Sales)), by = (DayOfWeek)]) +
+  geom_bar(aes(x = factor(DayOfWeek), y = sumSales))
+
+train[Sales != 0, .(sumSales = sum(Sales)), by = (DayOfWeek)]
+
+ggplot(train) + geom_bar(aes(x = factor(DayOfWeek), y = Sales, fill = DayOfWeek))
+
+ggplot(train, aes(x=factor(1), y=sales, fill = schoolholiday)) + 
+  geom_bar(width = 1, stat="identity") + coord_polar(theta="y")
+
+
+
+
+# Store
+# Store별 로그수, 매출 휴일 등은 원래 다를것임.
+length(unique(train$Store))
+
+## Sales 분포
 ggplot(data = train_data) + geom_histogram(aes(x = Sales), binwidth=500)
 ggplot(data = train_data[Sales < 500,]) + geom_histogram(aes(x = Sales), binwidth=10) # Sale 0인 상점-날짜가 꽤 있음.
 
